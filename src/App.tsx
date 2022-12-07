b90a39c01e34d0fdee605e92c00bf2b88091b6b5
TuanKietTran <kyletran101.work@gmail.com>
2022-12-07 22:19:13 +0700

    create views

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from "./views/Home";
 
 const App : React.FC = () => {
   return (
     <div className="App">
-      
+      <Home></Home>
     </div>
   );
 };
