b8a10637c5e070c290c209fbbadbaea888e13cb0
hao-phamlehoan <hao.phamlehoan@hcmut.edu.vn>
2022-12-13 13:27:45 +0700

    update record

--- src/components/Pagination/Record.jsx
+++ src/components/Pagination/Record.jsx
@@ -7,7 +7,7 @@ const Records = ({ data }) => {
         <div className="flex flex-wrap -m-4">
             {data.map((item, index) => (
                 <div key={index} className="xl:w-1/5 md:w-1/2 p-4">
-                    <Link to={`${item.product_type_code[0] =  item.uuid}`}>
+                    <Link to={`${item.product_type_code[0] === '1' ? '/book/'+item.uuid : '/stationery/'+item.uuid}`}>
                         <div className="bg-gray-100 p-6 rounded-lg h-96 flex flex-col justify-between">
                             <img className="h-40 rounded w-full object-cover object-center mb-6" src={item.cover} alt="content" />
                             <h2 className="text-lg text-gray-900 font-medium title-font mb-4 h-32">{item.p_name}</h2>
@@ -15,7 +15,7 @@ const Records = ({ data }) => {
                         </div>
                     </Link>
                 </div>
-            ))}
+            ))}z
         </div>
     );
 };
